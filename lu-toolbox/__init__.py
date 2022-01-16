0271c92a6f3e76b547208db794f5232ceb0c6b56
Bobbe <maxschlecht@web.de>
2022-01-16 20:02:36 +0100

    Make minimum supported blender version 2.93

--- lu-toolbox/__init__.py
+++ lu-toolbox/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
 	"name": "LU Toolbox",
 	"author": "Bobbe",
 	"version": (1, 7, 0),
-	"blender": (3, 0, 0),
+	"blender": (2, 93, 0),
 	"location": "3D View -> Sidebar -> LU Toolbox",
 	"category": "Import-Export",
 	"support": "COMMUNITY",
