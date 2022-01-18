f68f7d5f96da0db95a2c8903c8ace918e8f9a245
Bobbe <maxschlecht@web.de>
2022-01-18 17:17:50 +0100

    Fix weird bug with mesh splitting

--- lu_toolbox/divide_mesh.py
+++ lu_toolbox/divide_mesh.py
@@ -1,4 +1,4 @@
-import bpy
+import bpy, bmesh
 import numpy as np
 
 def divide_mesh(context, mesh_obj, max_verts=65536, min_div_rate=0.1):
@@ -17,13 +17,22 @@ def divide_mesh(context, mesh_obj, max_verts=65536, min_div_rate=0.1):
         bound_box = np.array(obj.bound_box)
         target_axis = np.argmax((bound_box[0] - bound_box[6]) ** 2)
         select = vecs[:,target_axis] < mean[target_axis]
-        mesh.vertices.foreach_set("select", select)
 
         bpy.ops.object.select_all(action="DESELECT")
         obj.select_set(True)
         context.view_layer.objects.active = obj
 
         bpy.ops.object.mode_set(mode="EDIT")
+        bpy.ops.mesh.select_all(action="DESELECT")
+
+        bm = bmesh.from_edit_mesh(mesh)
+        for vert, v_select in zip(bm.verts, select):
+            vert.select = v_select
+        bm.select_mode = {"VERT"}
+        bm.select_flush_mode()
+        bmesh.update_edit_mesh(mesh)
+        bm.free()
+
         bpy.ops.mesh.select_linked()
         bpy.ops.mesh.separate()
         bpy.ops.object.mode_set(mode="OBJECT")
@@ -34,7 +43,7 @@ def divide_mesh(context, mesh_obj, max_verts=65536, min_div_rate=0.1):
         div_rate = min(div_rate, 1 - div_rate)
 
         if div_rate < min_div_rate:
-            raise Exception(f"fatal: failed to divide mesh: {div_rate} < {min_div:rate} (div_rate < min_div_rate)")
+            raise Exception(f"fatal: failed to divide mesh: {div_rate} < {min_div_rate} (div_rate < min_div_rate)")
 
         return divide_rec(obj) + divide_rec(new_obj)
 
